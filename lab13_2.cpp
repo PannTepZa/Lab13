3b16e1b9eac22da9e4ce2733baeed439775f737a
PannTepZa <pannkitina@gmail.com>
2026-01-06 15:47:39 +0700

    13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M]){
+    for (int j = 0; j < M + 2; j++)
+        cout << "-";
+    for(int i = 0; i < N; i++){
+        cout << endl;
+        cout << "|";
+        for(int j = 0; j < M; j++){
+            if(image[i][j])
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "|" ;
+    }
+    cout << endl;
+    for (int j = 0; j < M + 2; j++){
+        cout << "-";
+    }
+    cout << endl;
+}
+
+void updateImage(bool image[][M],int s,int x,int y){
+    for(int i = x - s; i <= x + s; i++){
+        for(int j = y - s; j <= y + s; j++){
+            if(i >= 0 && i < N && j >= 0 && j < M){
+                if(sqrt((i - x) * (i - x) + (j - y) * (j - y)) <= s - 1){
+                    image[i][j] = true;
+                }
+            }
+        }
+    }
+}
